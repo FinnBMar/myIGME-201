e3a32acdfe6345fc1d7757e936f2efaa39ddde33
Finn Marable <fbm5192@g.rit.edu>
2023-09-01 19:05:05 -0400

    PE1 commit

---
+++ Marable_PE1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Marable_PE1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Finn Marable"); //prints my name to console
+            int number = 5;
+            Console.WriteLine(number + 10); //prints 10 + my previously initialized variable number (15)
+        }
+    }
+}
