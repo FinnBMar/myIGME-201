077312e548b74826a64f49261bb1acad0e80696f
Finn Marable <fbm5192@g.rit.edu>
2023-09-21 00:35:34 -0400

    pe8q8 push

--- PE8.Q8/Program.cs
+++ PE8.Q8/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PE8.Q8
 {
@@ -10,13 +6,29 @@ namespace PE8.Q8
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("enter a string");
+            Console.WriteLine("Enter a string");
             string userResponse = Console.ReadLine();
             userResponse = userResponse.Trim();
-            if (userResponse.ToLower() == "yes")
-            {
 
+            // Split the userResponse into an array of words
+            string[] words = userResponse.Split(' ');
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (words[i].ToLower() == "yes")
+                {
+                    words[i] = words[i].Replace("Yes", "No").Replace("yes", "no").Replace("YES", "NO").Replace("yEs", "nO").Replace("yeS", "nO").Replace("yEs", "nO");
+                }
+                else if (words[i].ToLower() == "no")
+                {
+                    words[i] = words[i].Replace("No", "Yes").Replace("no", "yes").Replace("NO", "YES").Replace("nO", "yES");
+                }
             }
+
+            // Join the modified words back into a single string
+            string modifiedUserResponse = string.Join(" ", words);
+
+            Console.WriteLine("Modified string: " + modifiedUserResponse);
         }
     }
 }
