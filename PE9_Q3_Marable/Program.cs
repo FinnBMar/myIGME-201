1ed159cbdc4af4b3c11b4cd9adebb0528f2293d9
Finn Marable <fbm5192@g.rit.edu>
2023-09-25 01:39:17 -0400

    commit for pe9

---
+++ PE9_Q3_Marable/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleInputImpersonation
+{
+    class Program
+    {
+        // Define a delegate for reading input
+        delegate string InputDelegate();
+
+        static void Main(string[] args)
+        {
+            InputDelegate inputMethod = Console.ReadLine;  // Create an instance of the delegate and assign it the Console.ReadLine() function
+            Console.WriteLine("Enter your name: ");
+            string userInput = inputMethod.Invoke(); // Use the delegate to read user input
+            Console.WriteLine($"Hello, {userInput}!");
+        }
+    }
+}
---
+++ PE9_Q4_Marable/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE9_Q4_Marable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Order newItem;
+            newItem.itemName = "wheaties";
+            newItem.unitCount = 5;
+            newItem.unitCost = 10;
+            Console.WriteLine(newItem.orderInfo());
+        }
+
+        struct Order
+        {
+            public string itemName;
+            public int unitCount;
+            public double unitCost;
+
+           
+            public double totalPrice ()
+            {
+                return (unitCount * unitCost);
+            }
+
+            public string orderInfo()
+            {
+                return ("Order Information: " + unitCount + " " + itemName + " items at $" + unitCost + " each, total cost $" + totalPrice());
+            }
+        }
+
+    }
+}
