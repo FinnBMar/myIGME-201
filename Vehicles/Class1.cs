3beeaefd4a12f7252759cbe4520961cb13bd93c8
Finn Marable <fbm5192@g.rit.edu>
2023-10-10 23:17:07 -0400

    PE 11 Q 5 Commit

---
+++ Vehicles/Class1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vehicles;
+
+namespace Vehicles
+{
+    public abstract class Vehicle
+    {
+        public virtual void LoadPassenger() { }
+    }
+
+    public abstract class Train : Vehicle
+    {
+        public override void LoadPassenger()
+        {
+
+        }
+    }
+    public interface IPassengerCarrier {
+        void LoadPassenger();
+    }
+    public interface IHeavyLoadCarrier
+    {
+
+    }
+    public abstract class Car : Vehicle
+    {
+        public override void LoadPassenger()
+        {
+
+        }
+    }
+    public class Compact : Car, IPassengerCarrier
+    {
+        public override void LoadPassenger()
+        {
+
+        }
+    }
+    public class SUV : Car, IPassengerCarrier
+    {
+        public override void LoadPassenger()
+        {
+
+        }
+    }
+    public class Pickup : Car, IPassengerCarrier, IHeavyLoadCarrier
+    {
+        public override void LoadPassenger()
+        {
+
+        }
+    }
+    public class PassengerTrain : Train, IPassengerCarrier
+    {
+        public override void LoadPassenger()
+        {
+
+        }
+    }
+    public class FreightTrain : Train, IHeavyLoadCarrier
+{
+        public override void LoadPassenger()
+        {
+
+        }
+    }
+    public class _424DoubleBogey : Train, IHeavyLoadCarrier
+    {
+        public override void LoadPassenger()
+        {
+
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
