7fcd78e1c57ca34dd08e2ceb3a946fe533d94854
Finn Marable <fbm5192@g.rit.edu>
2023-10-19 21:04:02 -0400

    PE 14 Actual Commit

---
+++ PE14/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+public interface IMyInterface //Finn Marable PE 14 This program calls myMethod for each object using the interface to ensure correct implementation.
+{
+    void MyMethod();
+}
+public class CustomClassA : IMyInterface
+{
+    public void MyMethod()
+    {
+        Console.WriteLine("CustomClassA implementation.");
+    }
+}
+public class CustomClassB : IMyInterface
+{
+    public void MyMethod()
+    {
+        Console.WriteLine("CustomClassB implementation.");
+    }
+}
+class Program
+{
+    public static void Main()
+    {
+        IMyInterface objectA = new CustomClassA();
+        IMyInterface objectB = new CustomClassB();
+
+        CallMyMethod(objectA);
+        CallMyMethod(objectB);
+    }
+    public static void CallMyMethod(object myObject)
+    {
+        if (myObject is IMyInterface)
+        {
+            ((IMyInterface)myObject).MyMethod();
+        }
+        else
+        {
+            Console.WriteLine("Object does not implement IMyInterface.");
+        }
+    }
+}
